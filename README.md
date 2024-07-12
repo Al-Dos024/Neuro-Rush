334b0fc3dcdcd1a0a111b6512fb7981ca2b3fe68
Ali Dosoqi <106039568+Al-Dos024@users.noreply.github.com>
2024-07-13 02:25:37 +0300

    create the app and repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# ADHD
-refactor of Flutter app that used as questioner for ADHD
---
+++ adhd/android/app/src/main/kotlin/com/example/adhd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adhd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
